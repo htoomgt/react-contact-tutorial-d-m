b0910d93be22ea826a1b66b4f2675fd473b164b8
htoo-mg-thait <htoomaungthait@gmail.com>
2021-06-01 13:29:39 +0630

    list, add and delete with axios

--- src/App.js
+++ src/App.js
@@ -7,32 +7,68 @@ import {v4 as uuid} from 'uuid';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import ContactDetail from './components/ContactDetail';
 import ContactDeleteConfirm from './components/ContactDeleteConfirm'
+import api from './api/Contact'
+
 
 
 function App() {
     const LOCAL_STORAGE_KEY = "contacts";
     const [contacts, setContacts] = useState([]);
 
-    const addContactHandler = (contact) => {
-        // console.log(contact);
-        setContacts([...contacts, {id: uuid(), ...contact}]);
+
+
+
+    //RetrieveContacts
+    const retrieveContacts = async () => {
+        const response = await api.get('/contacts');
+        return response.data;
+    }
+
+    const addContactHandler = async (contact) => {
+        console.log(contact);
+        const request = {
+            id : uuid(),
+            ...contact
+        }
+        const response =  await api.post("/contacts", request);
+        console.log(response);
+
+        setContacts([...contacts, response.data]);
     };
 
     useEffect(() => {
-        const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-        if(retriveContacts) setContacts(retriveContacts)
-    }, [])
+
+        // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        // if (retriveContacts) setContacts(retriveContacts);
+
+        const getAllCOntacts = async () => {
+            const allContacts = await retrieveContacts();
+            if (allContacts) setContacts(allContacts);
+        };
+
+        getAllCOntacts();
+    }, []);
 
     useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+        // localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
     }, [contacts])
 
-    const removeCotnactHandler = (id) => {
-        const newContactList = contacts.filter((contact)=> {
-            return contact.id !== id;
-        });
+    const removeCotnactHandler = async (id) => {
+        const response = await api.delete(`/contacts/${id}/`);
+        const getAllCOntacts = async () => {
+            const allContacts = await retrieveContacts();
+            if (allContacts) setContacts(allContacts);
+        };
 
-        setContacts(newContactList);
+        getAllCOntacts();
+
+        // const newContactList = contacts.filter((contact)=> {
+        //     return contact.id !== id;
+        // });
+        //
+        // setContacts(newContactList);
+
+
     }
 
   return (
